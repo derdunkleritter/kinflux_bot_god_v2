f0eab81e7c2772ca3d0ba4e1137e73e5cd2b272f
digglidy <143267069+digglidy@users.noreply.github.com>
2023-09-10 07:24:56 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['254102074064', Mose', true],
+  ['25112533252', 'Sly', true],
   ['254102074064', 'Mose', true], 
   [''] 
 ] //Number of owners
 
-global.mods = ['254110077535','254110377776','254765777423'] 
-global.prems = ['254110077535', '254110377776', '2250545752448']
+global.mods = ['25112533252','254102074064','254765777423'] 
+global.prems = ['25112533252', '254102074064', '2250545752448']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
